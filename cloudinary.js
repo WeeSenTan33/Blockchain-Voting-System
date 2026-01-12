575fa5944edf1eb91871105f5aefd3cc4c69f1d1
WeeSen Tan <104610028+WeeSenTan33@users.noreply.github.com>
2026-01-13 00:56:06 +0800

    Delete cloudinary.js

--- cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: 'dh7kqqopj',
-  api_key: '666872744882461',
-  api_secret: '2DltAtZr4RGguHAx5eUrIqGRD_E',
-});
-
-export default cloudinary;
